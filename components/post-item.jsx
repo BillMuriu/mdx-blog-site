5d31dc502bbcc0a3691690b6804da592a7736c1e
unknown <njorogekamau63@gmail.com>
2025-03-04 23:07:47 +0300

    styled the post item card

--- components/post-item.jsx
+++ components/post-item.jsx
@@ -1,21 +1,20 @@
 /**
  * PostItem Component
- * 
+ *
  * This component represents an individual blog post item.
  * It displays the post's title, description, and publication date.
  * The title is a clickable link that navigates to the post's detailed page.
  * The component also includes a "Read more" link for further navigation.
- * 
+ *
  * Props:
  * - slug (string): The URL slug for the post.
  * - title (string): The title of the post.
  * - description (string, optional): A brief description of the post.
  * - date (string): The publication date of the post.
- * 
+ *
  * Usage:
  * <PostItem slug="/post-url" title="Post Title" description="Post description" date="2024-07-11" />
  */
-
 
 import { Calendar } from "lucide-react";
 import Link from "next/link";
@@ -24,7 +23,7 @@ import { cn, formatDate } from "@/lib/utils";
 
 export function PostItem({ slug, title, description, date }) {
   return (
-    <article className="flex flex-col gap-2 border-border border-b py-3">
+    <article className="flex flex-col gap-2 border-border border mb-4 px-2 rounded-md py-3">
       <div>
         <h2 className="text-2xl font-bold">
           <Link href={slug}>{title}</Link>
