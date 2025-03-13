94a8772646c8802c80f1b01bd0d55aa09e27e354
unknown <njorogekamau63@gmail.com>
2025-03-13 20:52:39 +0300

    use solutions card for outlining the solutions

--- app/page.js
+++ app/page.js
@@ -4,16 +4,17 @@ import { cn, sortPosts } from "@/lib/utils";
 import { posts } from "#site/content";
 import Link from "next/link";
 import { PostItem } from "@/components/post-item";
+import { SolutionsCard } from "@/components/solutions-card";
 
 export default function Home() {
   const latestPosts = sortPosts(posts).slice(0, 5);
-  
+
   return (
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
         <div className="container flex flex-col gap-4 text-center">
-          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-            Hello, I&apos;m James
+          <h1 className="text-3xl sm:text-3xl md:text-6xl lg:text-5xl font-black text-balance">
+            Implement custom ai workflows to your business
           </h1>
           <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
             Welcome to my blog template. Built using tailwind, shadcn, velite
@@ -24,9 +25,9 @@ export default function Home() {
               href="/blog"
               className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
             >
-              View my blog
+              Book a free dicovery call
             </Link>
-            <Link
+            {/* <Link
               href={siteConfig.links.github}
               target="_blank"
               rel="noreferrer"
@@ -36,9 +37,15 @@ export default function Home() {
               )}
             >
               GitHub
-            </Link>
+            </Link> */}
           </div>
         </div>
+      </section>
+      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
+        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
+          Solutions
+        </h2>
+        <SolutionsCard />
       </section>
       <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
         <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
--- config/site.js
+++ config/site.js
@@ -1,12 +1,11 @@
 export const siteConfig = {
-    name: "Morioh Blog",
-    url: "https://example.com",
-    description: "Nextjs 14 blog using velite, tailwind and shadcn",
-    author: "Bill Morioh",
-    links: {
-      twitter: "https://twitter.com/jollyshopland",
-      github: "https://github.com/jolbol1",
-      personalSite: "https://jamesshopland.com",
-    },
-  };
-  
+  name: "Bill Morio",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Bill Morioh",
+  links: {
+    twitter: "https://twitter.com/jollyshopland",
+    github: "https://github.com/jolbol1",
+    personalSite: "https://jamesshopland.com",
+  },
+};
